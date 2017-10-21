d380fc514e49b7d465891a1ffbc7311db523d4b5
JoshuaSG3161 <32961794+JoshuaSG3161@users.noreply.github.com>
2017-10-20 22:49:01 -0400

    Update Offseason.java

--- Offseason.java
+++ Offseason.java
@@ -1,15 +1,9 @@
-/**My computer is having trouble using the import function for the WPI libraies so that might be wrong.
-*/
-
 package offseaon.java;
-
-public class Offseaon {
 
 /** This is for the 2017 code package. Thiscode is not on github
  * I randomly created a package and when i know what the package name is, 
  * i will fix it then
  */
-	package org.usfirst.frc.team3161.robot;
 
 	import edu.wpi.first.wpilibj.DoubleSolenoid;
 
@@ -17,8 +11,8 @@ public class Offseaon {
 	/**Making new class and indetifying new Varibles
 	*/
 
-	public class Gear extends Hopper {
-		
+
+	public class Offseaon {
 	  private static final Gear INSTANCE = new Gear();
 	  
 	  /**Making The Pneumatics start in rest mode
@@ -33,11 +27,9 @@ public class Offseaon {
 		private final DoubleSoleniod Claw = new DoubleSolenoid(1, 2);
 		private final DoubleSolonoid Flap = new DoubelSolenoid(3, 4);
 
-		public static Gear getInsatnce() {
-			return INSTANCE;
-	}
+		
 
-		public void disableAll() {
+		{
 	        Flap.set(DoubleSolenoid.Value.kOff);
 	        Claw.set(DoubleSolenoid.Value.kOff);
 		boolean disabled = true;
