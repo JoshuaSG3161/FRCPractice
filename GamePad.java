98776f6e048e5f747f967b7eaf8a71e7e2d270bd
JoshuaSG3161 <32961794+JoshuaSG3161@users.noreply.github.com>
2017-10-24 17:27:19 -0400

    Update GamePad.java

--- GamePad.java
+++ GamePad.java
@@ -1,16 +1,20 @@
 /**For Operator GamePad and Controls
-  *This is to be inputed into the Robot.java file for the 2017 code. This is just part of 
-  *Code that needs to be added on.
-  */
+	  *This is to be inputed into the Robot.java file for the 2017 code. This is just part of 
+	  *Code that needs to be added on.
+	  */
 
-  import edu.wpi.first.wpilibj.DoubleSolenoid;
-  import ca.team3161.interfaces.GearMech;
-      
-       LogitechDualAction operatorpad = new LogitechDualAction(1, 50, TimeUnit.MILLISECONDS);
-       registerLifecycleComponent(operatorPad);
+		import edu.wpi.first.wpilibj.DoubleSolenoid;
+		import ca.team3161.interfaces.GearMech;
+		
+		Claw claw = new DoubleSolenoid (1,2);
+		Flap flap = new DoubleSolenoid (3,4);
+		GearMech gearMech = new GearMech(claw, flap);
+	      
+		LogitechDualAction operatorpad = new LogitechDualAction(1, 50, TimeUnit.MILLISECONDS);
+		registerLifecycleComponent(operatorpad);
 
-      
-      	operatorPad.bind(LogitechButton.X, PressType.PRESS  gearMech::openClaw);
-		operatorPad.bind(LogitechButton.X, PressType.RELEASE gearMech::cl
-      	operatorPad.bind(LogitechButton.B, PressType.PRESS gearMech::openFlap);
-        operatorPad.bind(LogitechButton.B, PressType.RELEASE gearMech::closeFlap)
+	      
+		operatorpad.bind(LogitechButton.X, PressType.PRESS  gearMech::openClaw);
+		operatorpad.bind(LogitechButton.X, PressType.RELEASE gearMech::closeClaw);
+		operatorpad.bind(LogitechButton.B, PressType.PRESS gearMech::openFlap);
+		operatorpad.bind(LogitechButton.B, PressType.RELEASE gearMech::closeFlap);
