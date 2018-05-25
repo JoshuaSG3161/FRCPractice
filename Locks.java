b317ace56be98d216c36b0ed6c18ac0103a3e8c5
JoshuaSG3161 <32961794+JoshuaSG3161@users.noreply.github.com>
2018-05-25 09:43:43 -0400

    Update Locks.java

--- Locks.java
+++ Locks.java
@@ -1,45 +1,29 @@
 package Basics_201;
-
-import java.util.Scanner;
-
 public class Locks {
-	 public int password;
-	 private boolean locked;
-	 
-	 Locks Locky = new Locks();
-	 Locks Lockette = new Locks();
-	 
-	 public Locks() {
-	     password = (int)(Math.random() * 500);
-	     System.out.println(password);
-	     locked = true;
-			boolean JoshisCodeGod = true; {
-			while(JoshisCodeGod) {
-			
-				 Locks Locky = new Locks();
-				 Locks Lockette = new Locks();
-				
-			Scanner input = new Scanner(System.in);
-			
-			int comLocky;
-			int comLockette;
-
-			for(int i = 0; i < 2; i++) {
-				if (comLocky == password) {
-					System.out.println("You found the Combination!");
-					break;
-				}else 
-					System.out.println("You Suck");	
-					
-											} 
-			for(int i = 0; i < 2; i++) {
-				if (comLockette == password) {
-					System.out.println("You found the Combination!");
-					break;
-				}else 
-					System.out.println("You Suck");	
-								}
-			}
-		}
+     public int password;
+     private boolean locked;
+     
+     Locks Locky = new Locks();
+     Locks Lockette = new Locks();
+     
+     public Locks() {
+         password = (int)(Math.random() * 500);
+         System.out.println(password);
+         locked = true;
+     }
+     
+     public boolean getLocked() {
+         //Returns if the lock is locked
+         return this.locked;
+     }
+     
+     public void openLock(int pass) {
+         //User attepmts to unlock the lock
+         if(pass == this.password) {
+             //Changes boolean value inside 'this' lock
+             this.locked = false;
+             System.out.print("Unlocked");
+           
+         }
  }  
 }
