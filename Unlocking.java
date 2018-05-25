d15749e47e8ef1533ae8bf5ad35b9f66eb3f45e0
JoshuaSG3161 <32961794+JoshuaSG3161@users.noreply.github.com>
2018-05-25 09:44:27 -0400

    Update Unlocking.java

--- Unlocking.java
+++ Unlocking.java
@@ -1,35 +1,40 @@
 package Basics_201;
-
 import java.util.Scanner;
-
 public class Unlocking extends Locks {
-	public static int comLocky;
-	public static int comLockette;
-
-	public static void main(String[] args) {
-		
-				boolean JoshisCodeGod = true; {
-				while(JoshisCodeGod) {
-				
-					 Locks Locky = new Locks();
-					 Locks Lockette = new Locks();
-					
-				Scanner input = new Scanner(System.in);
-			
-
-				for(int i = 0; i < 2; i++) {
-					System.out.println(i);
-					System.out.print("Please enter a number for Lock 1.... ");
-					comLocky = input.nextInt();
-					
-												} 
-				for(int i = 0; i < 2; i++) {
-					System.out.println(i);
-					System.out.print("Please enter a number for Lock 2.... ");
-					comLockette = input.nextInt();
-					
-				}
-			}
-		}
-	}
-}
+    public static void main(String[] args) {
+        
+                boolean JoshisCodeGod = true; {
+                while(JoshisCodeGod) {
+                
+                     Locks Locky = new Locks();
+                     Locks Lockette = new Locks();
+                    
+                Scanner input = new Scanner(System.in);
+                
+                int comLocky;
+                int comLockette;
+                for(int i = 0; i < 2; i++) {
+                    System.out.println(i);
+                    System.out.print("Please enter a number for Lock 1.... ");
+                    comLocky = input.nextInt();
+                    if (comLocky == password) {
+                        System.out.println("You found the Combination!");
+                        break;
+                    }else 
+                        System.out.println("You Suck");   
+                        
+                                                } 
+                for(int i = 0; i < 2; i++) {
+                    System.out.println(i);
+                    System.out.print("Please enter a number for Lock 2.... ");
+                    comLockette = input.nextInt();
+                    if (comLockette == password) {
+                        System.out.println("You found the Combination!");
+                        break;
+                    }else 
+                        System.out.println("You Suck");   
+                                    }
+                }
+            }
+        }
+        }
